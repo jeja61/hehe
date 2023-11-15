37cbc4dc55bf81ad99591ad4c8d3630713b5f49c
Yuliya Tsapaeva <tsapaeva@clearskylab.co>
2023-11-15 21:30:08 +0300

    12ц

--- scr.js
+++ scr.js
@@ -1,141 +1,56 @@
-const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-const scratchCardCover = document.querySelector('.scratch-card-cover');
-const scratchCardCanvasRender = document.querySelector('.scratch-card-canvas-render');
-const scratchCardCoverContainer = document.querySelector('.scratch-card-cover-container');
-const scratchCardText = document.querySelector('.scratch-card-text');
-const scratchCardImage = document.querySelector('.scratch-card-image');
-
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
-let isPointerDown = false;
-let positionX;
-let positionY;
-let clearDetectionTimeout = null;
-
-const devicePixelRatio = window.devicePixelRatio || 1;
-
-const canvasWidth = canvas.offsetWidth * devicePixelRatio;
-const canvasHeight = canvas.offsetHeight * devicePixelRatio;
-
-canvas.width = canvasWidth;
-canvas.height = canvasHeight;
-
-context.scale(devicePixelRatio, devicePixelRatio);
-
-if (isSafari) {
-  canvas.classList.add('hidden');
-}
-
-canvas.addEventListener('pointerdown', (e) => {
-  scratchCardCover.classList.remove('shine');
-  ({ x: positionX, y: positionY } = getPosition(e));
-  clearTimeout(clearDetectionTimeout);
+/**
+ * Initially the elements are positioned in front of the camera.
+ * We define here the transformations to position them in the scene.
+ * When clicking on an item, we apply the inversed transformation but on the main container to "zoom" on the item
+ */
+const articles = new Map([
+    ["6392322", { tx: "-90%", tz: "-70vmin", ry: "60deg" }],
+    ["1761279", { tz: "-110vmin" }],
+    ["1679772", { tx: "90%", tz: "-70vmin", ry: "-60deg" }]
+  ]);
   
-  canvas.addEventListener('pointermove', plot);
+  window.addEventListener("load", () => {
+    const main = document.querySelector("main");
   
-  window.addEventListener('pointerup', (e) => {
-    canvas.removeEventListener('pointermove', plot);
-    clearDetectionTimeout = setTimeout(() => {
-      checkBlackFillPercentage();
-    }, 500);
-  }, { once: true });
-});
-
-const checkBlackFillPercentage = () => {
-  const imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
-  const pixelData = imageData.data;
-
-  let blackPixelCount = 0;
-
-  for (let i = 0; i < pixelData.length; i += 4) {
-    const red = pixelData[i];
-    const green = pixelData[i + 1];
-    const blue = pixelData[i + 2];
-    const alpha = pixelData[i + 3];
-
-    if (red === 0 && green === 0 && blue === 0 && alpha === 255) {
-      blackPixelCount++;
+    for (const [id, { tx, tz, ry }] of articles.entries()) {
+      main.appendChild(makeArticleElement(id, tx, tz, ry));
     }
-  }
-
-  const blackFillPercentage = blackPixelCount * 100 / (canvasWidth * canvasHeight);
- 
-  if (blackFillPercentage >= 45) {
-    scratchCardCoverContainer.classList.add('clear');
-    confetti({
-      particleCount: 100,
-      spread: 90,
-      origin: {
-         y: (scratchCardText.getBoundingClientRect().bottom + 60) / window.innerHeight,
-      },
+  
+    document.addEventListener("click", ({ target }) => {
+      const targetId = target.closest("article")?.id;
+      let [itx, itz, iry] = [0, 0, 0]; // inversed transformation to apply to the main element
+  
+      if (targetId && main.dataset.focus !== targetId) {
+        // zoom in
+        const { tx, tz, ry } = articles.get(targetId) || {};
+        [itx, itz, iry] = [tx, tz, ry].map(inverseTransformation);
+        main.setAttribute("data-focus", targetId);
+      } else {
+        // zoom out
+        main.removeAttribute("data-focus");
+      }
+      main.style.transform = `rotateY(${iry}) translate3d(${itx}, 0, ${itz})`;
     });
-    scratchCardText.textContent = '🎉 You got a $50 Apple gift card!';
-    scratchCardImage.classList.add('animate');
-    scratchCardCoverContainer.addEventListener('transitionend', () => {
-      scratchCardCoverContainer.classList.add('hidden');
-    }, { once: true });
-  }
-}
-
-const getPosition = ({ clientX, clientY }) => {
-  const { left, top } = canvas.getBoundingClientRect();
-  return {
-    x: clientX - left,
-    y: clientY - top,
-  };
-}
-
-const plotLine = (context, x1, y1, x2, y2) => {
-  var diffX = Math.abs(x2 - x1);
-  var diffY = Math.abs(y2 - y1);
-  var dist = Math.sqrt(diffX * diffX + diffY * diffY);
-  var step = dist / 50;
-  var i = 0;
-  var t;
-  var x;
-  var y;
-
-  while (i < dist) {
-    t = Math.min(1, i / dist);
-
-    x = x1 + (x2 - x1) * t;
-    y = y1 + (y2 - y1) * t;
-
-    context.beginPath();
-    context.arc(x, y, 16, 0, Math.PI * 2);
-    context.fill();
-
-    i += step;
-  }
-}
-
-const setImageFromCanvas = () => {
-  canvas.toBlob((blob) => {
-    const url = URL.createObjectURL(blob);
-    previousUrl = scratchCardCanvasRender.src;
-    scratchCardCanvasRender.src = url;
-    if (!previousUrl) {
-      scratchCardCanvasRender.classList.remove('hidden');
-    } else {
-      URL.revokeObjectURL(previousUrl);
-    }
-    previousUrl = url;
   });
-}
-
-let setImageTimeout = null;
-
-const plot = (e) => {
-  const { x, y } = getPosition(e);
-  plotLine(context, positionX, positionY, x, y);
-  positionX = x;
-  positionY = y;
-  if (isSafari) {
-    clearTimeout(setImageTimeout);
-
-    setImageTimeout = setTimeout(() => {
-      setImageFromCanvas();
-    }, 5);
+  
+  // e.g. turn "90%" into "-90%" or "-10vmin" into "10vmin"
+  function inverseTransformation(transform) {
+    if (!transform) return 0;
+    const [_, value, unit] = transform.match(/(-?\d+)(.*)/);
+    return `${-Number(value)}${unit}`;
   }
-};
+  
+  function makeArticleElement(id, tx = 0, tz = 0, ry = 0) {
+    const img = document.createElement("img");
+    img.src = `${id}.jpg`;
+  
+    const button = document.createElement("button");
+    button.appendChild(img);
+  
+    const element = document.createElement("article");
+    element.id = id;
+    element.style.transform = `translate3d(${tx}, 0, ${tz}) rotateY(${ry})`;
+    element.appendChild(button);
+    return element;
+  }
+  
